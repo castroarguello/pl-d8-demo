aa14a2ecd4377e3bb1853a96d675ceab659dddeb
Ramiro Castro Arguello <castroarguello@gmail.com>
2017-11-06 15:19:02 -0300

    fix app

--- src/ui/src/app/app.module.ts
+++ src/ui/src/app/app.module.ts
@@ -2,12 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { NgbdAccordionBasic } from './accordion-basic';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    NgbdAccordionBasic,
     AppComponent
   ],
   imports: [
