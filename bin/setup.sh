8d5c0605007661dfaa63cd5ceb63d6317a56fec3
GDrupal <musicaoriginal@gmail.com>
2017-11-09 15:35:43 -0300

    Update setup script to create a default install with jsonapi and demo content.

--- bin/setup.sh
+++ bin/setup.sh
@@ -3,3 +3,22 @@
 # --unsafe-perm ensures dispatch to theme-related operations can still run as root for Docker.
 docker-compose -f build.yml run --rm cli "npm install --unsafe-perm && grunt --timer --quiet"
 
+docker-compose -f build.yml run --rm cli 'cd /var/www/build/html && ../../vendor/drupal/console/bin/drupal site:install standard  \
+--langcode="en"  \
+--db-type="mysql"  \
+--db-host="db"  \
+--db-name="d8_headless"  \
+--db-user="admin"  \
+--db-pass="admin"  \
+--db-port="3306"  \
+--site-name="Drupal 8 headless"  \
+--site-mail="admin@example.com"  \
+--account-name="admin"  \
+--account-mail="admin@example.com"  \
+--account-pass="admin" --force'
+
+docker-compose -f build.yml run --rm cli 'cd /var/www/build/html && ../../vendor/drupal/console/bin/drupal moi jsonapi devel_generate'
+
+docker-compose -f build.yml run --rm drush genc 20
+
+
