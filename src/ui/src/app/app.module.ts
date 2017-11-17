dc64c6c05e4d73d47fd85691a839870df3e2db37
GDrupal <musicaoriginal@gmail.com>
2017-11-17 15:19:09 -0300

    Remove redundant 404 path.

--- src/ui/src/app/app.module.ts
+++ src/ui/src/app/app.module.ts
@@ -14,7 +14,6 @@ import { ArticleService } from './article.service';
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
-  { path: '404', component: NotFoundComponent },
   { path: '**', component: NotFoundComponent }
 ];
 
